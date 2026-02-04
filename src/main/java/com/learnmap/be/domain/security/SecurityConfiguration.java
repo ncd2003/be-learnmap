4ca75a9e2d267b3aef3bdc1ba83d9be1788d31b4
NGUYEN CANH DUONG <duongnche173192@fpt.edu.vn>
2026-02-04 07:37:52 +0700

    add cors config in security

--- src/main/java/com/learnmap/be/domain/security/SecurityConfiguration.java
+++ src/main/java/com/learnmap/be/domain/security/SecurityConfiguration.java
@@ -16,6 +16,7 @@ public class SecurityConfiguration {
             , CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         String[] whiteList = {"/", "api/v1/auth/login", "api/v1/auth/refresh", "api/v1/auth/register", "api/v1/course/public", "api/v1/category/public", "api/v1/course/public/category/**","api/v1/plan/public","api/v1/topic/public","api/v1/comment/public/**","api/v1/post/public/**"};
         http
+                .cors(Customizer.withDefaults())
                 // Tắt CSRF để tránh lỗi khi gọi API
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
